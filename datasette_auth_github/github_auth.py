7fd2ec174d799265830e094be62b125dc6019689
Simon Willison <swillison@gmail.com>
2019-07-13 16:29:47 -0700

    GitHub logo on 'Log in with GitHub' - closes #42

--- datasette_auth_github/github_auth.py
+++ datasette_auth_github/github_auth.py
@@ -21,6 +21,30 @@ body {
 }</style>
 """
 
+LOGIN_BUTTON = """
+<a href="{}" target="_top" style="
+  text-decoration: none;
+  font-family: helvetica;
+  font-weight: bold;
+  color: #ddd;
+  background-color: black;
+  height: 30px;
+  display: inline-block;
+  padding: 1px 12px 0 0;
+  border-radius: 5px;
+">
+<svg xmlns="http://www.w3.org/2000/svg"
+aria-label="GitHub" role="img"
+viewBox="0 0 512 512" style="
+  width: 30px; vertical-align: middle; border-right:0.5px solid #aaa;"
+><rect
+width="512" height="512" rx="15%" fill="#1B1817"/>
+<path fill="#fff" d="M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-50c-71 16-86-28-86-28-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z"/>
+</svg>
+<span style="padding-left: 5px;">Log in with GitHub</span>
+</a>
+"""
+
 
 class GitHubAuth:
     cookie_name = "asgi_auth"
@@ -280,8 +304,8 @@ class GitHubAuth:
         ):
             await send_html(
                 send,
-                """{}<h1>Logged out</h1><p><a href="{}">Log in with GitHub</a></p>""".format(
-                    LOGIN_CSS, github_login_url
+                """{}<h1>Logged out</h1><p>{}</p>""".format(
+                    LOGIN_CSS, LOGIN_BUTTON.format(github_login_url)
                 ),
                 headers=cookie_headers,
             )
